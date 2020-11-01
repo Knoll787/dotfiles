5e29a16af7fbe69097694b2356d656534885cbaf
Knoll787 <noel.woods@protonmail.com>
2020-11-01 17:09:50 +1100

    working colorscheme, based off drcula in st

--- dwm/.config/dwm/dwm.c
+++ dwm/.config/dwm/dwm.c
@@ -9,8 +9,7 @@
  * The event handlers of dwm are organized in an array which is accessed
  * whenever a new event has been fetched. This allows event dispatching
  * in O(1) time.
- *
- * Each child of the root window is called a client, except windows which have
+ * * Each child of the root window is called a client, except windows which have
  * set the override_redirect flag. Clients are organized in a linked client
  * list on each monitor, the focus history is remembered through a stack list
  * on each monitor. Each client contains a bit array to indicate the tags of a
