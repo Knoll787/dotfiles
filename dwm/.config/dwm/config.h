932a2cf52759ce851e30bc186857bd7bb0dea0a0
Knoll787 <noel.woods@protonmail.com>
2020-12-20 15:37:10 +1100

    Minor updates

--- dwm/.config/dwm/config.h
+++ dwm/.config/dwm/config.h
@@ -1,10 +1,10 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
-static const int showbar            = 1;        /* 0 means no bar */
+static const int swallowfloating    = 1;        /* 1 means swallow floating windows by default */
+static const int showbar            = 0;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
@@ -12,7 +12,7 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#0975E1";
+static const char col_cyan[]        = "#9933ff";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -28,8 +28,6 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
-	{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
-	{ "brave",   NULL,     NULL,           1 << 8,    0,          1,           0,        -1 },
 	{ "st",      NULL,     NULL,           0,         0,          1,           0,        -1 },
 	{ NULL,      NULL,     "Event Tester", 0,         1,          0,           1,        -1 }, /* xev */
 };
@@ -41,13 +39,13 @@ static const int resizehints = 0;    /* 1 means respect size hints in tiled resi
 #include "fibonacci.c"
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-  { "[S]",      spiral },
+	{ "[S]",      spiral },
 	{ "[T]",      tile },    /* first entry is default */
-  { "[M]",      centeredmaster },
-  { "[D]",      dwindle },
+	{ "[M]",      centeredmaster },
+	{ "[D]",      dwindle },
 	{ "[F]",      NULL },    /* no layout function means floating behavior */
 	{ "[0]",      monocle },
-  { NULL,       NULL },
+	{ NULL,       NULL },
 };
 
 /* key definitions */
@@ -77,12 +75,12 @@ static Key keys[] = {
 	{ MODKEY,                       XK_p,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-  { MODKEY,                       XK_s,      togglesticky,   {0} },
+	{ MODKEY,                       XK_s,      togglesticky,   {0} },
 	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY,                       XK_q,      killclient,     {0} },
-  { MODKEY|ShiftMask,		          XK_j,      cyclelayout,    {.i = -1 } },
-  { MODKEY|ShiftMask,             XK_k,      cyclelayout,    {.i = +1 } },
+	{ MODKEY|ShiftMask,		          XK_j,      cyclelayout,    {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_k,      cyclelayout,    {.i = +1 } },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
